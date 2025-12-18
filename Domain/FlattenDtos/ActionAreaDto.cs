3e010350cbafc88f9cac88254239e5073b1b46d1
fireilya <fireilya3000@gmail.com>
2025-12-18 16:05:31 +0500

    accadiently broke all and fixed it

--- Domain/FlattenDtos/ActionAreaDto.cs
+++ Domain/FlattenDtos/ActionAreaDto.cs
@@ -2,9 +2,10 @@ using System;
 
 namespace Domain.FlattenDtos
 {
-    public record ActionAreaDto(Guid Id, string PathToTexture)
+    public record ActionAreaDto(Guid Id, string PathToTexture, string Name)
     {
         public Guid Id { get; set; } = Id;
         public string PathToTexture { get; set; } = PathToTexture;
+        public string Name { get; set; } = Name;
     }
 }
