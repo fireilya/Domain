08ae6debae553de19c32a1c7494a4b2563c3eca6
Daniil <sgktost1@gmail.com>
2025-12-18 23:51:13 +0500

    icon dto

---
+++ Domain/FlattenDtos/IconDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.FlattenDtos
+{
+    public record IconDto(
+        string Id,
+        byte[] Body
+    )
+    {
+        public string Id { get; set; } = Id;
+        public byte[] Body { get; set; } = Body;
+    }
+}
