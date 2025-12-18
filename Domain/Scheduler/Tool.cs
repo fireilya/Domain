54a12b2bb1f56ad9d412e387df9032c240377ee1
Daniil <sgktost1@gmail.com>
2025-12-18 20:33:32 +0500

    Добавил квест айди в инструмент

--- Domain/Scheduler/Tool.cs
+++ Domain/Scheduler/Tool.cs
@@ -2,9 +2,10 @@ using System;
 
 namespace Domain.Scheduler
 {
-    public record Tool(Guid Id, string Name, string PathToIcon)
+    public record Tool(Guid Id, Guid QuestId, string Name, string PathToIcon)
     {
         public Guid Id { get; } = Id;
+        public Guid QuestId { get; set; } = QuestId;
         public string Name { get; } = Name;
         public string PathToIcon { get; } = PathToIcon;
     }
