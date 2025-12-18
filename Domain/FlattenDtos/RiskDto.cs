fa5e71f2266dba84d58d88c83a0a52874af169df
fireilya <fireilya3000@gmail.com>
2025-12-18 23:28:18 +0500

    Risk updated

--- Domain/FlattenDtos/RiskDto.cs
+++ Domain/FlattenDtos/RiskDto.cs
@@ -2,9 +2,16 @@ using System;
 
 namespace Domain.FlattenDtos
 {
-    public record RiskDto(Guid Id, Guid NeutralizerId, Guid LocationId)
+    public record RiskDto(
+        Guid Id,
+        string PathToIcon,
+        string Description,
+        Guid NeutralizerId,
+        Guid LocationId)
     {
         public Guid Id { get; set; } = Id;
+        public string PathToIcon { get; set; } = PathToIcon;
+        public string Description { get; set; } = Description;
         public Guid NeutralizerId { get; set; } = NeutralizerId;
         public Guid LocationId { get; set; } = LocationId;
     }
--- Domain/Scheduler/Risk.cs
+++ Domain/Scheduler/Risk.cs
@@ -4,10 +4,14 @@ namespace Domain.Scheduler
 {
     public record Risk(
         Guid Id,
+        string PathToIcon,
+        string Description,
         Neutralizer Neutralizer
     )
     {
         public Guid Id { get; } = Id;
+        public string PathToIcon { get; } = PathToIcon;
+        public string Description { get; } = Description;
         public double Chance { get; } = 0.5;
         public Neutralizer Neutralizer { get; } = Neutralizer;
         public bool IsNeutralizerFound { get; set; }
