d12f855992f0e403a0620bb296650b8640a8515c
fireilya <fireilya3000@gmail.com>
2025-12-18 21:23:41 +0500

    allow null neededTool

--- Domain/Scheduler/ProcessSubtask.cs
+++ Domain/Scheduler/ProcessSubtask.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Domain.Scheduler
 {
-    public record ProcessSubtask(Guid Id, string Name, int BaseEfficiency, bool IsUseCapacityTool, Guid NeededToolId) :
+    public record ProcessSubtask(Guid Id, string Name, int BaseEfficiency, bool IsUseCapacityTool, Guid? NeededToolId) :
         Subtask(Id, Name, BaseEfficiency, IsUseCapacityTool)
     {
         public double WorkCoeff => (ChosenTool != NeededToolId ? 0.5 : 1.0);
-        public Guid NeededToolId { get; } = NeededToolId;
+        public Guid? NeededToolId { get; } = NeededToolId;
         public Guid? ChosenTool { get; set; } = null;
         public override int Efficiency => (int)(BaseEfficiency * WorkCoeff);
     }
