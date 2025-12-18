a127c1e0d5780a34d08765fce620c663beb47a69
Daniil <sgktost1@gmail.com>
2025-12-18 20:14:28 +0500

    nullable resource id

--- Domain/FlattenDtos/SubtaskCapacityToolDto.cs
+++ Domain/FlattenDtos/SubtaskCapacityToolDto.cs
@@ -2,10 +2,10 @@ using System;
 
 namespace Domain.FlattenDtos
 {
-    public record SubtaskCapacityToolDto(Guid Id, Guid ResourceId, Guid SubtaskId, short Capacity)
+    public record SubtaskCapacityToolDto(Guid Id, Guid? ResourceId, Guid SubtaskId, short Capacity)
     {
         public Guid Id { get; set; } = Id;
-        public Guid ResourceId { get; set; } = ResourceId;
+        public Guid? ResourceId { get; set; } = ResourceId;
         public Guid SubtaskId { get; set; } = SubtaskId;
         public short Capacity { get; set; } = Capacity;
     }
