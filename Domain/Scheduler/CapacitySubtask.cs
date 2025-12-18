ddd4c2c10a0da6e24722a73960510a6a21571d64
Daniil <sgktost1@gmail.com>
2025-12-18 19:58:41 +0500

    ещё клинап

--- Domain/Scheduler/CapacitySubtask.cs
+++ Domain/Scheduler/CapacitySubtask.cs
@@ -4,16 +4,16 @@ using System.Collections.Generic;
 namespace Domain.Scheduler
 {
     public record CapacitySubtask(
-        Guid Id, 
-        string Name, 
-        int BaseEfficiency, 
+        Guid Id,
+        string Name,
+        int BaseEfficiency,
         bool IsUseCapacityTool,
-        int BaseCapacity, 
-        IReadOnlyDictionary<Guid, short> ToolsCapacity) :
-        Subtask(Id, Name, BaseEfficiency, IsUseCapacityTool)
+        int BaseCapacity,
+        IReadOnlyDictionary<Guid, short> ToolsCapacity
+    ) : Subtask(Id, Name, BaseEfficiency, IsUseCapacityTool)
     {
         public int BaseCapacity { get; } = BaseCapacity;
-        public IReadOnlyDictionary<Guid, short> ToolsCapacity { get; } =  ToolsCapacity;
+        public IReadOnlyDictionary<Guid, short> ToolsCapacity { get; } = ToolsCapacity;
         public Tool? ChosenTool { get; set; } = null;
 
         public override int Efficiency => BaseEfficiency;
